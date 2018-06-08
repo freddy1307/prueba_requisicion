408dcf25e41d74922108ffa6d7e745673600c444
Your Name <lavalencia@gi.com>
2018-06-08 13:09:49 -0500

    Agregue los comentarios al modelo

--- models/quotitation.py
+++ models/quotitation.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-
-import logging
-
+## @package models
+#  Documentation for this module.
+#  Modelo para la vista de cotizacion junto con la pestaña de lineas
+#  More details.
 from odoo import models, fields, api, _
 
-
 class Quotitation(models.Model):
+    """Documentation for a class.
+        Clase que crea los campos con sus tipos de datos para el encabezado de la vista de cotizacion
+        """
     _name = 'gi.quotation'
     _description = 'Example quotation'
 
@@ -17,11 +20,18 @@ class Quotitation(models.Model):
 
     @api.model
     def create(self, vals):
+        """Documentation for a method.
+            Metodo create el cual altera la accion de guardar para que antes de que se guarde
+            en la BD agregue al campo name el valor que le corresponde de su secuencia
+        """
         vals['name'] = self.env['ir.sequence'].next_by_code('gi.quotation') or _('New')
         return super(Quotitation, self).create(vals)
 
 
 class QuotationLine(models.Model):
+    """Documentation for a class.
+        Clase que crea los campos con sus tipos de datos para las lineas de la vista de cotizacion
+        """
     _name = 'gi.quotation.line'
     _description = 'Line quotation'
 
@@ -34,6 +44,9 @@ class QuotationLine(models.Model):
 
     @api.depends('line_id')
     def get_number(self):
+        """Documentation for a method.
+            Metodo compute el cual genera el numero siguiente de la linea
+        """
         for lines in self.mapped('line_id'):
             number = 1
             for line in lines.line_ids:
@@ -43,6 +56,9 @@ class QuotationLine(models.Model):
     @api.onchange('qty','price_list')
     @api.depends('qty','price_list')
     def calc_total(self):
+        """Documentation for a method.
+            Metodo que hace el onchange y calculo del precio de lista por la cantidad para sacar el total de la linea
+        """
         for rec in self:
             if rec.qty > 0 and rec.price_list > 0:
                 rec.total_line = rec.qty * rec.price_list
